6b0776e19d85f9be116da16b19622380b70acd00
anclark686 <anclark686@gmail.com>
2023-07-25 20:42:32 -0400

    adjusting CORS

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Backend
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
-        origins 'localhost:5173', 'https://reyaly-financial-planner-f6c880178bcd.herokuapp.com'
+        origins 'localhost:5173', 'https://main.d1r7v6lmapahca.amplifyapp.com'
         resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
       end
     end
