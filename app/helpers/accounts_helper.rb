61d6418f4778a25ab3359c7e30640de72521caef
anclark686 <anclark686@gmail.com>
2023-07-26 02:12:39 -0400

    removing stray print statements

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,9 +1,8 @@
 module AccountsHelper
     def clean_other_accounts(account, expenses)
         for i in expenses do
-            puts "in the accounts controller"
-            puts i
             other_accounts = i.account_ids
+
             for num in other_accounts.length.downto(0) do
                 if other_accounts[num] != account._id 
                     puts "other=#{other_accounts[num]} - main=#{account._id}"
@@ -17,9 +16,6 @@ module AccountsHelper
     end
 
     def remove_expense(account, expense)
-        puts "heyyyy welcome"
-        puts account
-        puts 
         begin
             other_acct = Account.find_by(id: account)
             other_acct.pull(expense_ids: expense._id)
@@ -27,10 +23,5 @@ module AccountsHelper
             puts err
         end
         
-        puts @account
-        puts 
-        
-        puts expense._id
-        puts 
     end
 end
